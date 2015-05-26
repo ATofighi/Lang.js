6bfb0b5ed2d36c1f261245aabd69027cf4b3e2a7
ATofighi <alirtofighim@gmail.com>
2015-05-27 03:29:48 +0430

    Reformating the code

--- src/lang.js
+++ src/lang.js
@@ -1,45 +1,42 @@
 var Lang = {
 	languages: {},
 
-	error: function(message)
-	{
+	error: function (message) {
 		console.error('Lang.js', message);
 	},
 
-	warn: function(message)
-	{
+	warn: function (message) {
 		console.error('Lang.js', message);
 	},
 
-	log: function(message)
-	{
+	log: function (message) {
 		console.log('Lang.js', message);
 	},
-	
-	helpers: {
-		pregQuote: function preg_quote(str, delimiter)
-		{
-			  //  discuss at: http://phpjs.org/functions/preg_quote/
-			  // original by: booeyOH
-			  // improved by: Ates Goral (http://magnetiq.com)
-			  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-			  // improved by: Brett Zamir (http://brett-zamir.me)
-			  // bugfixed by: Onno Marsman
-			  //   example 1: preg_quote("$40");
-			  //   returns 1: '\\$40'
-			  //   example 2: preg_quote("*RRRING* Hello?");
-			  //   returns 2: '\\*RRRING\\* Hello\\?'
-			  //   example 3: preg_quote("\\.+*?[^]$(){}=!<>|:");
-			  //   returns 3: '\\\\\\.\\+\\*\\?\\[\\^\\]\\$\\(\\)\\{\\}\\=\\!\\<\\>\\|\\:'
 
-			  return String(str)
+	helpers: {
+		pregQuote: function preg_quote(str, delimiter) {
+			//  discuss at: http://phpjs.org/functions/preg_quote/
+			// original by: booeyOH
+			// improved by: Ates Goral (http://magnetiq.com)
+			// improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+			// improved by: Brett Zamir (http://brett-zamir.me)
+			// bugfixed by: Onno Marsman
+			//   example 1: preg_quote("$40");
+			//   returns 1: '\\$40'
+			//   example 2: preg_quote("*RRRING* Hello?");
+			//   returns 2: '\\*RRRING\\* Hello\\?'
+			//   example 3: preg_quote("\\.+*?[^]$(){}=!<>|:");
+			//   returns 3: '\\\\\\.\\+\\*\\?\\[\\^\\]\\$\\(\\)\\{\\}\\=\\!\\<\\>\\|\\:'
+
+			return String(str)
 				.replace(new RegExp('[.\\\\+*?\\[\\^\\]$(){}=!<>|:\\' + (delimiter || '') + '-]', 'g'), '\\$&');
 		},
-		
-		objectSort: function(list, func)
-		{
-			if(!func) {
-				func = function(a, b) {return a[1] > b[1]};
+
+		objectSort: function (list, func) {
+			if (!func) {
+				func = function (a, b) {
+					return a[1] > b[1]
+				};
 			}
 			var sortable = [], newList = {};
 			for (var item in list) {
@@ -49,85 +46,83 @@ var Lang = {
 			for (var item in sortable) {
 				newList[sortable[item][0]] = sortable[item][1];
 			}
-			
+
 			return newList;
 		},
-		
-		treeToObject: function(obj, newObj, prefix) {
-			if(!prefix) {
+
+		treeToObject: function (obj, newObj, prefix) {
+			if (!prefix) {
 				prefix = '';
 			}
-			if(!newObj) {
+			if (!newObj) {
 				newObj = {};
 			}
-			for(var item in obj) {
-				if(typeof obj[item] == 'object') {
+			for (var item in obj) {
+				if (typeof obj[item] == 'object') {
 					newObj = Lang.helpers.treeToObject(obj[item], newObj, prefix + item + '.');
 				} else {
-					newObj[prefix+item] = obj[item];
+					newObj[prefix + item] = obj[item];
 				}
 			}
 			return newObj;
 		}
 	},
-	
+
 	/**
-	 * @param object languages
+	 * @param languages
 	 *
 	 * @return void
 	 */
-	addResource: function(languages, prefix)
-	{
-		if(!prefix) {
+	addResource: function (languages, prefix) {
+		if (!prefix) {
 			prefix = 'lang.';
 		}
-		for(language in languages) {
-			if(typeof languages[language] == 'string') {
+		for (language in languages) {
+			if (typeof languages[language] == 'string') {
 				Lang.languages[prefix + language] = languages[language];
 			}
-			else if(typeof languages[language] == 'object') {
+			else if (typeof languages[language] == 'object') {
 				Lang.addResource(languages[language], prefix + language + '.');
 			} else {
 				Lang.warn('`languages` param must be object/string')
 			}
 		}
 	},
-	
+
 	/**
 	 *
 	 * @return void
 	 */
-	clear: function()
-	{
+	clear: function () {
 		Lang.languages = {};
 	},
-	
+
 	/**
-	 * @param string langCode
+	 * @param langCode
 	 *
 	 * @return boolean
 	 */
-	has: function(langCode) {
+	has: function (langCode) {
 		return typeof Lang.languages['lang.' + langCode] != 'undefined';
 	},
-	
+
 	/**
-	 * @param string message
-	 * @param object variables
+	 * @param message
+	 * @param variables
 	 *
 	 * @return string
 	 */
-	trans: function(message, variables) {		
-		if(typeof variables != 'object') {
+	trans: function (message, variables) {
+		if (typeof variables != 'object') {
 			variables = {};
 		}
-		
+
 		variables = Lang.helpers.treeToObject(variables);
-		
-		variables = Lang.helpers.objectSort(variables, function(a, b) {
+
+		variables = Lang.helpers.objectSort(variables, function (a, b) {
 			return a[0] < b[0];
 		});
-		for(variable in variables) {
+		for (variable in variables) {
 			var code = ':' + variable.toLowerCase(), regex;
 			code = Lang.helpers.pregQuote(code);
 			regex = new RegExp(code, 'gi');
@@ -135,16 +130,16 @@ var Lang = {
 		}
 		return message;
 	},
-	
+
 	/**
-	 * @param string langCode
-	 * @param object variables
+	 * @param langCode
+	 * @param variables
 	 *
 	 * @return string
 	 */
-	get: function(langCode, variables) {
+	get: function (langCode, variables) {
 		var message;
-		if(!Lang.has(langCode)) {
+		if (!Lang.has(langCode)) {
 			message = langCode;
 		}
 		else {
@@ -154,51 +149,51 @@ var Lang = {
 	},
 
 	/**
-	 * @param string langCode
-	 * @param integer count
-	 * @param object variables
+	 * @param langCode
+	 * @param count
+	 * @param variables
 	 *
 	 * @return string
 	 */
-	choice: function(langCode, count, variables) {
+	choice: function (langCode, count, variables) {
 		var message;
-		if(!Lang.has(langCode)) {
+		if (!Lang.has(langCode)) {
 			message = langCode;
 		}
 		else {
 			message = Lang.languages['lang.' + langCode];
 		}
-		if(!variables) {
+		if (!variables) {
 			variables = {};
 		}
-		if(!variables['count']) {
+		if (!variables['count']) {
 			variables.count = count;
 		}
-		
+
 		choices = message.split('|');
 		var i = 1;
-		for(choice in choices) {
+		for (choice in choices) {
 			var choiceMin, choiceMax, match;
-			if(match = choices[choice].match(/^\[([\-0-9a-zA-Z]+),\s?([\-0-9a-zA-Z]+)\]\s?/i)) {
+			if (match = choices[choice].match(/^\[([\-0-9a-zA-Z]+),\s?([\-0-9a-zA-Z]+)\]\s?/i)) {
 				choiceMin = match[1];
 				choiceMax = match[2];
 				choices[choice] = choices[choice].substr(match[0].length);
 			}
-			else if(match = choices[choice].match(/^\{([\-0-9a-zA-Z]+)}\s?/i)) {
+			else if (match = choices[choice].match(/^\{([\-0-9a-zA-Z]+)}\s?/i)) {
 				choiceMin = choiceMax = match[1];
 				choices[choice] = choices[choice].substr(match[0].length);
 			}
 			else {
 				choiceMin = choiceMax = i;
-				if(choice == 0) {
+				if (choice == 0) {
 					choiceMin = 'Inf';
 				}
-				else if(choice == choices.length-1) {
+				else if (choice == choices.length - 1) {
 					choiceMax = 'Inf';
 				}
 			}
 
-			if((choiceMin.toString().match('Inf') || choiceMin <= count) && (choiceMax.toString().match('Inf') || choiceMax >= count)) {
+			if ((choiceMin.toString().match('Inf') || choiceMin <= count) && (choiceMax.toString().match('Inf') || choiceMax >= count)) {
 				return Lang.trans(choices[choice], variables);
 			}
 			i++;
