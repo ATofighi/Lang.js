1d0934ec3e85d14857c97d197a930f02f3076cf2
AliReza Tofighi Mohammadi <alirtofighim@gmail.com>
2017-11-07 17:12:49 +0330

    Add TypeScript definitions

--- src/lang.d.ts
+++ src/lang.d.ts
@@ -1,30 +1,26 @@
 // Type definitions for Lang.js
 // Project: https://github.com/ATofighi/Lang.js
+declare var Lang: Lang.ILangStatic;
+declare namespace Lang {
+	interface ILangStatic {
+		languages: any;
 
-interface ILang {
-	languages: any;
+		error(message: string): void;
 
-	error(message: string): void;
+		warn(message: string): void;
 
-	warn(message: string): void;
+		log(message: string): void;
 
-	log(message: string): void;
+		addResource(languages: string[] | object[], prefix?: string): void;
 
-	addResource(languages: string[] | object[], prefix?: string): void;
+		clear(): void;
 
-	clear(): void;
+		has(langCode: string): boolean;
 
-	has(langCode: string): boolean;
+		trans(message: string, variables?: object);
 
-	trans(message: string, variables?: object);
+		get(langCode: string, variables?: object);
 
-	get(langCode: string, variables?: object);
-
-	choice(langCode: string, count: number, variables: object);
-}
-
-declare let Lang: ILang;
-
-declare module "Lang" {
-	export default Lang;
+		choice(langCode: string, count: number, variables: object);
+	}
 }
